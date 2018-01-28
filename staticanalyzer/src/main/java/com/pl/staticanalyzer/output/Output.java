52d25852b7e04e4314f3697a9de89ebfb86c7492
pawel <7proPS7@gmail.com>
2018-01-28 14:25:16 +0100

    #7 - Replace Map with MultiMap and write Test

--- staticanalyzer/src/main/java/com/pl/staticanalyzer/output/Output.java
+++ staticanalyzer/src/main/java/com/pl/staticanalyzer/output/Output.java
@@ -1,15 +1,19 @@
 package com.pl.staticanalyzer.output;
 
-import static com.pl.staticanalyzer.GlobalHashMap.map;
+import com.pl.staticanalyzer.report.ReportType;
+
+import static com.pl.staticanalyzer.GlobalHashMap.reportMessages;
 import static com.pl.staticanalyzer.report.ReportType.ERROR;
 import static com.pl.staticanalyzer.report.ReportType.WARNING;
 
 public class Output {
-
     public void writeOnConsole() {
-        map.get(ERROR).forEach(val -> System.out.println("[" + ERROR + "] " + val));
-        System.out.println();
-        map.get(WARNING).forEach(val -> System.out.println("[" + WARNING + "] " + val));
-        map.clear();
+        writeMessageByReportType(ERROR);
+        writeMessageByReportType(WARNING);
+        reportMessages.clear();
+    }
+
+    private void writeMessageByReportType(ReportType reportType) {
+        reportMessages.get(reportType).forEach(val -> System.out.println("[" + ERROR + "] " + val));
     }
 }
--- staticanalyzer/src/main/java/com/pl/staticanalyzer/report/Report.java
+++ staticanalyzer/src/main/java/com/pl/staticanalyzer/report/Report.java
@@ -1,22 +1,22 @@
 package com.pl.staticanalyzer.report;
 
-import java.util.List;
+import java.util.Collection;
 
-import static com.pl.staticanalyzer.GlobalHashMap.map;
+import static com.pl.staticanalyzer.GlobalHashMap.reportMessages;
 import static com.pl.staticanalyzer.report.ReportType.ERROR;
 import static com.pl.staticanalyzer.report.ReportType.WARNING;
 
 public class Report {
 
-    public List<String> get(ReportType type) {
-        return map.get(type);
+    public Collection<String> get(ReportType type) {
+        return reportMessages.get(type);
     }
 
     public void addWarning(String value) {
-        map.get(WARNING).add(value.trim());
+        reportMessages.put(WARNING, value.trim());
     }
 
     public void addError(String value) {
-        map.get(ERROR).add(value.trim());
+        reportMessages.put(ERROR, value.trim());
     }
 }
